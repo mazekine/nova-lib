6d294eee3cc22a15adf4c1a7f551b0dcbec6e3fa
cryonyx <vp@mazekine.com>
2020-12-14 14:51:40 +0300

    Wrapped POST /transfer

---
+++ src/main/kotlin/com/broxus/nova/models/InternalTransaction.kt
@@ -0,0 +1,12 @@
+package com.broxus.nova.models
+
+import com.google.gson.annotations.Expose
+
+/**
+ * Output model from sendTransfer method
+ *
+ * @property id Id of transaction. UUID ver. 4 rfc
+ */
+data class InternalTransaction(
+    @Expose val id: String
+)
